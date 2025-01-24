45d02d8533340e78243d1730fde84edc9b888da6
chandan200209 <chandangta963@gmail.com>
2025-01-24 10:37:57 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import FilterBar from "./components/FilterBar"
-import Footer from "./components/Footer"
-import Header from "./components/header"
-import ProductGrid from "./components/ProductGrid"
-import SectionHeader from "./components/SectionHeader"
+import FilterBar from "./components/FilterBar";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ProductGrid from "./components/ProductGrid";
+import SectionHeader from "./components/SectionHeader";
 
 function App() {
-
-  return(
-    <>    
-      <Header/>
-      <SectionHeader/>
-      <FilterBar/>
-      <ProductGrid/>
-      <Footer/>
+  return (
+    <>
+      <Header />
+      <SectionHeader />
+      <FilterBar />
+      <ProductGrid />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
